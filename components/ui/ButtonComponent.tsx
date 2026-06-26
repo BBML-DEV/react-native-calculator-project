323cfbb518a07c5dc387e44ea51f151977cd06e5
Paulo Vitor <66692202+BBML-DEV@users.noreply.github.com>
2026-06-25 21:49:17 -0300

    criando componente de botão

---
+++ components/ui/ButtonComponent.tsx
@@ -0,0 +1,14 @@
+import { TouchableHighlight } from "react-native"
+
+type props = {
+    buttonTextValue: string,
+    functionCalculate: () => void
+}
+
+export default function ButtonComponent ({buttonTextValue, functionCalculate}: props){
+    return (
+        <TouchableHighlight onPress={functionCalculate}>
+            <text className="text-[40px] h-[calc(100vw/4)] w-[calc(100vw/4)] p-5 bg-[#f0f0f0] text-center border border-[#888]">{buttonTextValue}</text>
+        </TouchableHighlight>
+    )
+}
